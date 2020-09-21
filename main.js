d62f017719575e8b779978e749d376b57589b6cb
marnade <marnade@users.noreply.github.com>
2020-09-21 16:47:29 -0400

    Modifie texte

--- main.js
+++ main.js
@@ -1,1 +1,10 @@
-console.log("Hello world");
+import figlet from "figlet";
+
+figlet("Damn son,\nwhere'd'th\nyou find this?", function (err, data) {
+	if (err) {
+		console.log("Something went wrong...");
+		console.dir(err);
+		return;
+	}
+	console.log(data);
+});
